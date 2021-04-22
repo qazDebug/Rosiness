73fc03b44f9ecae36221b2415b50b4832cf70164
杨明 <>
2021-04-22 11:36:15 +0800

    **

--- Assets/Rosiness/Scripts/Runtime/Engine/Engine.Utility/MonoUtility.cs
+++ Assets/Rosiness/Scripts/Runtime/Engine/Engine.Utility/MonoUtility.cs
@@ -8,21 +8,44 @@ public class MonoUtility : MonoBehaviour
 {
     #region Button
 
-    public static Button FindButtonAndSetOnClick(Transform transform, string strPath = null,
-        System.Action onClick = null, bool repeat = true)
+    public static Button FindButtonAndSetOnClick(Transform transform, string strPath = null, System.Action onClick = null, bool repeat = true)
     {
         if (transform == null) return null;
         Button button = null;
-        if (string.IsNullOrEmpty(strPath)) button = transform.GetComponent<Button>();
-        else button = GOUtility.FindObject<Button>(transform, strPath);
+        button = string.IsNullOrEmpty(strPath) ? transform.GetComponent<Button>() : GOUtility.FindObject<Button>(transform, strPath);
         
         if (button != null) button.onClick.AddListener(() =>
         {
-            
+            if (!repeat)
+            {
+                if(IsButtonRepeatClick(button)) return;
+            }
 
             onClick?.Invoke();
         });
-        return null;
+        return button;
+    }
+
+    private static bool IsButtonRepeatClick(Button btnCheck)
+    {
+        if (btnCheck == null) return true;
+        if (!btnCheck.enabled) return true;
+        btnCheck.enabled = false;
+        return false;
     }
     #endregion
+
+    public static GameObject RemoveCanvas(GameObject myObject)
+    {
+        if (myObject == null) return myObject;
+
+        Canvas addCanvas = myObject.GetComponent<Canvas>();
+        if (addCanvas != null)
+        {
+            GraphicRaycaster graphicRaycaster = myObject.GetComponent<GraphicRaycaster>();
+            if(graphicRaycaster != null) Destroy(graphicRaycaster);
+            Destroy(addCanvas);
+        }
+        return myObject;
+    }
 }
